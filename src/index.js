00294146650a53d4e024bdf16da884d9d832023f
ravinainn <nain.ravi12345@gmail.com>
2023-10-14 20:40:04 +0530

    error resolve

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App';
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 // import Front_content from './components/Python';
-import "./style/style.css"
-import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
-import Main from './components/Home/Main';
-import Footer from './components/Footer';
+import "./style/style.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./components/Home/Main";
+import Footer from "./components/Footer";
 // import Partners from './components/Home/Partners';
 // import Super10batch from './components/Home/Super10batch';
-import Home from './components/Home/Home';
-import Super10 from './components/Super-10/Super10';
-import Page from './components/post/discuss/Page';
-import Login from './login/pages/login';
-import Register from './login/pages/Register';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Home from "./components/Home/Home";
+import Super10 from "./components/Super-10/Super10";
+import Page from "./components/post/discuss/Page";
+import Login from "./login/pages/login";
+import Register from "./login/pages/Register";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <Navbar/>
+    <Navbar />
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/Super-10" element={<Super10/>}/>
-      <Route path="/page" element={<Page/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-
+      <Route path="/" element={<Home />} />
+      <Route path="/Super-10" element={<Super10 />} />
+      <Route path="/page" element={<Page />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
-    <Footer/>
+    <Footer />
   </Router>
 );
-
